fcbc841b5eb7f21401bed51f488cdf3ac8de27ac
DemetriusJayme <115664765+DemetriusJayme@users.noreply.github.com>
2022-11-10 11:44:01 -0300

    quinta

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-//App.js
+//App.js - É o pai da aplicação
 
 import { useState } from 'react';
 import './App.css';
 import foods from './foods.json';
+import FoodBox from './components/FoodBox.js';
 
 function App() {
-  const [allFoods, setAllFoods] = useState(foods);
+  //useState é como o react quer que a gente crie nossas variáveis
+  //const [variável, função que altera o valor da variável] = useState(valorInicial)
+  //useState
 
+  const [allFoods, setAllFoods] = useState(foods);
+  const [food, setFood] = useState({
+    name: 'Orange',
+    calories: 85,
+    image: 'https://i.imgur.com/abKGOcv.jpg',
+    servings: 1,
+  })
+
+
+  //return que fica na mesma linha não precisa de ()
+  //No JSX
   return (
+    //useState
     <div className="App">
+      <FoodBox food={food} nomeDaProp={'karen'} />
+
       {allFoods.map((food) => {
         return (
-          <div>
+          <div key={food.name}>
             <p> {food.name} </p>
-            <img src={food.image} width={50} alt={food.name} />
+            <img src={food.image} width={100} alt={food.name} />
           </div>
         );
       })}
---
+++ src/components/FoodBox.js
@@ -0,0 +1,26 @@
+//Todo componente é uma função
+//È uma função que retorna HTML (JSX)
+import { Card, Col, Button } from 'antd';
+
+function FoodBox({ food }) {
+  //console.log('estou dentro');
+
+  return (
+    <Col>
+      <Card
+        title={'FOOD_NAME_HERE'}
+        style={{ width: 230, height: 300, margin: 10 }}
+      >
+        <img src={'FOOD_IMAGE_HERE'} height={60} alt="food" />
+        <p>Calories: FOOD_CALORIES_HERE</p>
+        <p>Servings: FOOD_SERVINGS_HERE</p>
+        <p>
+          <b>Total Calories: FOOD_CALORIES * FOOD_SERVINGS </b> kcal
+        </p>
+        <Button type="primary"> Delete </Button>
+      </Card>
+    </Col>
+  );
+}
+
+export default FoodBox;
